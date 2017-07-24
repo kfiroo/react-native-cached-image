0c1195970386da243ff28bc6e333008bffb978fb
kfiroo <kfirgolan@gmail.com>
2017-07-24 14:02:10 +0300

    Upgrade to RN 0.46

--- CachedImageExample/android/app/src/main/java/com/cachedimageexample/MainApplication.java
+++ CachedImageExample/android/app/src/main/java/com/cachedimageexample/MainApplication.java
@@ -4,12 +4,14 @@ import android.util.Log;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+
 import com.facebook.soloader.SoLoader;
+
+import com.RNFetchBlob.RNFetchBlobPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -39,6 +41,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
+    }
+
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
 
     @Override
