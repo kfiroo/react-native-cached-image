7ee2531336c4b35aee022a22eb6f5d7c5e2dd428
kfiroo <kfirgolan@gmail.com>
2017-08-09 16:28:03 +0300

    use const image ref

--- CachedImage.js
+++ CachedImage.js
@@ -52,7 +52,7 @@ class CachedImage extends React.Component {
     };
 
     static defaultProps = {
-            renderImage: props => (<ImageBackground imageStyle={props.style} ref={'cachedImage'} {...props} />),
+            renderImage: props => (<ImageBackground imageStyle={props.style} ref={CACHED_IMAGE_REF} {...props} />),
             activityIndicatorProps: {},
     };
 
