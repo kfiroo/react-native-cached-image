19aee2f4156386ca4b9182813e8c0a3162dbd4c2
Daniel Salinas <zzorba@users.noreply.github.com>
2019-11-03 10:10:53 -0600

    Rename deprecated methods to suppress warnings (for now) (#8)
    * Rename deprecated methods to suppress warnings (for now)
    
    * Mark CachedImage as unsafe as well
    
    * well don't stop there, completed search and replace for UNSAFE_componentWillMount

--- CachedImage.js
+++ CachedImage.js
@@ -77,7 +77,7 @@ class CachedImage extends React.Component {
         this.renderLoader = this.renderLoader.bind(this);
     }
 
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
         this._isMounted = true;
         NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
         // initial
@@ -96,7 +96,7 @@ class CachedImage extends React.Component {
         NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
     }
 
-    componentWillReceiveProps(nextProps) {
+    UNSAFE_componentWillReceiveProps(nextProps) {
         if (!_.isEqual(this.props.source, nextProps.source)) {
             this.processSource(nextProps.source);
         }
