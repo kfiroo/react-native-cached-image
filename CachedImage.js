e85b470bcff43e9b6bc159e6148ae83fe46850ed
Gary Fung <fungilation@gmail.com>
2019-12-08 17:02:56 -0800

    simplified deprecated netinfo code, now removed in react-native-netinfo v5

--- CachedImage.js
+++ CachedImage.js
@@ -79,21 +79,13 @@ class CachedImage extends React.Component {
 
     UNSAFE_componentWillMount() {
         this._isMounted = true;
-        NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
-        // initial
-        NetInfo.isConnected.fetch()
-            .then(isConnected => {
-                this.safeSetState({
-                    networkAvailable: isConnected
-                });
-            });
-
+        this.netinfoUnsubscribe = NetInfo.addEventListener(this.handleConnectivityChange);
         this.processSource(this.props.source);
     }
 
     componentWillUnmount() {
         this._isMounted = false;
-        NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+        this.netinfoUnsubscribe();
     }
 
     UNSAFE_componentWillReceiveProps(nextProps) {
@@ -131,7 +123,7 @@ class CachedImage extends React.Component {
         return this.setState(newState);
     }
 
-    handleConnectivityChange(isConnected) {
+    handleConnectivityChange({ isConnected }) {
         this.safeSetState({
             networkAvailable: isConnected
         });
