ff5601a517d3f3434c3406ca949961dec8e0d3bf
abiralgtm <abiralgautam19@gmail.com>
2020-04-10 20:20:47 +0545

    netinfo import from react native community library

--- CachedImage.js
+++ CachedImage.js
@@ -16,10 +16,11 @@ const {
     View,
     ImageBackground,
     ActivityIndicator,
-    NetInfo,
     Platform,
     StyleSheet,
 } = ReactNative;
+
+const NetInfo = require('@react-native-community/netinfo');
 
 const styles = StyleSheet.create({
     image: {
